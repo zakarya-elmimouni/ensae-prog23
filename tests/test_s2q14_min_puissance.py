3ec1f27089881a22f314322dd153561f49534d65
Zakarya elmimouni <zakartazaki2002@gmail.com>
2023-04-06 18:20:51 +0200

    last

--- tests/test_s2q14_min_puissance.py
+++ tests/test_s2q14_min_puissance.py
@@ -1,16 +1,25 @@
 import sys 
 sys.path.append("delivery_network")
 
-from graph import graph_from_file
+from graph import graph_from_file,kruskal
 import unittest   # The test framework
 
 class Test_MinimalPower(unittest.TestCase):
     def test_network12(self):
         g = graph_from_file("input/network.12.in")
-        self.assertEqual(g.puissance_min(1, 4)[1], 3)
-        self.assertEqual(g.puissance_min(1, 5)[1],3 )
-        self.assertEqual(g.puissance_min(3, 7)[1],3)
-        self.assertEqual(g.puissance_min(8, 5)[1],1)
-       
+        arbrecouvrant=kruskal(g)
+        self.assertEqual(g.puissance_min(arbrecouvrant,1, 4)[1], 3)
+        self.assertEqual(g.puissance_min(arbrecouvrant,1, 5)[1],3 )
+        self.assertEqual(g.puissance_min(arbrecouvrant,3, 7)[1],3)
+        self.assertEqual(g.puissance_min(arbrecouvrant,8, 5)[1],1)
+    def test_network0(self):
+        g = graph_from_file("input/network.00.in")
+        arbrecouvrant=kruskal(g)
+        self.assertEqual(g.puissance_min(arbrecouvrant,1, 4)[1], 11)
+        self.assertEqual(g.puissance_min(arbrecouvrant,2, 4)[1], 10)
+    def test_network1(self):
+        g = graph_from_file("input/network.04.in")
+        arbrecouvrant=kruskal(g)
+        self.assertEqual(g.puissance_min(arbrecouvrant,1, 4)[1], 4)     
 if __name__ == '__main__':
     unittest.main()
